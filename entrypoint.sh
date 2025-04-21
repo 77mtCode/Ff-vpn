4419244cab4f69713cab1e9aabb8ea01b72ce15b
77mtCode <bsfarm57@gmail.com>
2025-04-21 01:52:20 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Adiciona 100ms de delay no tráfego da porta do Free Fire (por exemplo, 10000-10010)
-tc qdisc add dev eth0 root netem delay 150ms 40ms loss 1%
+# Adiciona lag apenas nas portas usadas pelo Free Fire (exemplo 10000–10010)
+tc qdisc add dev eth0 root handle 1: prio
+tc qdisc add dev eth0 parent 1:3 handle 30: netem delay 150ms 40ms loss 1%
 
-# Inicia o WireGuard
+tc filter add dev eth0 protocol ip parent 1:0 prio 3 u32 match ip dport 10000 0xfff0 flowid 1:3
+
+# Sobe o WireGuard
 wg-quick up wg0
 
 # Mantém o container rodando
