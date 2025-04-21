af1f3c871968195a2495a876cb25d6fd13efc329
77mtCode <bsfarm57@gmail.com>
2025-04-21 02:12:20 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Adiciona lag apenas nas portas usadas pelo Free Fire (exemplo 10000–10010)
-tc qdisc add dev eth0 root handle 1: prio
-tc qdisc add dev eth0 parent 1:3 handle 30: netem delay 150ms 40ms loss 1%
+tc qdisc add dev eth0 root netem delay 120ms 20ms distribution normal
 
 tc filter add dev eth0 protocol ip parent 1:0 prio 3 u32 match ip dport 10000 0xfff0 flowid 1:3
 
