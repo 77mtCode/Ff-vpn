3317db1e6e8d8cb0b669582359208a5f49f5ebc5
77mtCode <bsfarm57@gmail.com>
2025-04-21 01:46:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Adiciona 100ms de delay no tráfego da porta do Free Fire (por exemplo, 10000-10010)
-tc qdisc add dev eth0 root netem delay 100ms
+tc qdisc add dev eth0 root netem delay 150ms 40ms loss 1%
 
 # Inicia o WireGuard
 wg-quick up wg0
